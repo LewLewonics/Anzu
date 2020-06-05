cf0f0b3540a0570fde9e361828bdf4d47b30100d
LewLewonics <lewlewonics@gmail.com>
2020-06-04 19:53:24 -0700

    first commit

---
+++ js/home.js
@@ -0,0 +1,19 @@
+const books = document.querySelectorAll('.book-container');
+
+console.log(books);
+
+books.forEach( book => {
+    book
+    .querySelector('.book-cover')
+    .addEventListener('click', () => {
+        let already_active = false;
+        //Append active class to book info
+        if (book.querySelector('.book-info').classList.contains('active')) already_active = true;
+
+        const book_info = document.querySelectorAll('.book-info')
+        book_info.forEach(infopage => infopage.classList.remove('active'));
+
+        if (already_active == true) book.querySelector('.book-info').classList.remove('active');
+        else book.querySelector('.book-info').classList.add('active');
+    })
+});
